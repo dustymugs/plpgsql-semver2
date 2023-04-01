36d5e7e2418f80ea547bb1f6bf016f7fd9cc92e6
Bborie Park <dustymugs@gmail.com>
2023-04-01 08:33:20 -0700

    more test

--- tests/test.eq.sql
+++ tests/test.eq.sql
@@ -4,3 +4,4 @@ SELECT text_to_semver('1.2.3-alpha.beta') = text_to_semver('1.2.3-alpha.beta'); 
 SELECT text_to_semver('1.2.3-alpha.beta.1') = text_to_semver('1.2.3-alpha.beta.1'); -- TRUE
 SELECT text_to_semver('1.2.3-alpha.beta.1+amd64') = text_to_semver('1.2.3-alpha.beta.1+amd64'); -- TRUE
 SELECT text_to_semver('1.2.3+amd64') = text_to_semver('1.2.3+amd64'); -- TRUE
+SELECT text_to_semver('1.2.3+build1') = text_to_semver('1.2.3+build2'); -- TRUE
